5a77a668a25f0af0975047718c6a4bc8c58d616c
Ryan Token <ryantoken13@gmail.com>
2020-08-18 09:09:22 -0500

    Adjusted formatting for the About Us picture

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,8 +1,8 @@
 var plugins = [{
-      plugin: require('/Users/home/Dev/Web/Projects/thegoldenhurricast/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/tokenr/Local/Dev/Side/thegoldenhurricast/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/home/Dev/Web/Projects/thegoldenhurricast/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/tokenr/Local/Dev/Side/thegoldenhurricast/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"The Golden Hurricast","short_name":"TGH","start_url":"/","background_color":"#002D72","theme_color":"#002D72","display":"standalone","icon":"static/apple-touch-icon-180x180.webp","crossOrigin":"use-credentials","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"eb94fd3b4da582fbad1e1d168cc2ca58"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
