30dd012f9ea59d9c7517cf5f890605dec11aef1e
Ryan Token <ryantoken13@gmail.com>
2020-08-31 20:52:53 -0500

    Removed Pat from the 'About Us' description :(

--- src/components/AboutJumbotron.jsx
+++ src/components/AboutJumbotron.jsx
@@ -10,7 +10,7 @@ const AboutJumbotron = () => {
           <Container className="container_style" style={{backgroundColor: 'rgba(0, 0, 0, 0.5)', borderRadius: '15px', padding: '15px'}}>
             <h1>About Us</h1>
             <p style={{ fontSize: "18px" }}>
-                The Golden Hurricast is an independent podcast and blog that covers all things Golden Hurricane athletics by Ryan Token, Matt Rechtien, and Pat Fox.
+                The Golden Hurricast is an independent podcast and blog that covers all things Golden Hurricane athletics by Ryan Token and Matt Rechtien.
             </p>
             <p style={{ fontSize: "18px" }}>
                 We’re three TU alums, friends, and die-hard fans who love talking TU sports. Not affiliated with The University of Tulsa.
