0b50317a9519b49d7f6724abb10c52573ffc33aa
Ryan Token <ryantoken13@gmail.com>
2020-09-02 22:23:52 -0500

    Trying to figure out why an image isn't showing up for social links

--- src/templates/blogPost.jsx
+++ src/templates/blogPost.jsx
@@ -29,7 +29,7 @@ const Template = ({data, pageContext}) => {
         <SEO
           title={title}
           description={excerpt || ''}
-          image={"../../static/logo-white.jpg"}
+          image={"../../public/logo-white.jpg"}
           pathname={path}
           article
         />
