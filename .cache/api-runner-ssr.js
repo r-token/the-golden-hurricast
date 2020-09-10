6f6e3017ef62d2cad4151009647f6ccf02ba8f6d
Ryan Token <ryantoken13@gmail.com>
2020-09-09 22:36:56 -0500

    Updated the 'Previous 4 Episodes' section of the Podcast page

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,8 +1,8 @@
 var plugins = [{
-      plugin: require('/Users/home/Dev/Web/Projects/thegoldenhurricast/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/rtoken/Dev/Web/Projects/thegoldenhurricast/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/home/Dev/Web/Projects/thegoldenhurricast/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/rtoken/Dev/Web/Projects/thegoldenhurricast/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"The Golden Hurricast","short_name":"TGH","start_url":"/","background_color":"#002D72","theme_color":"#002D72","display":"standalone","icon":"static/apple-touch-icon.webp","crossOrigin":"use-credentials","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"25536b73763a79fd4857f010bced2939"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
