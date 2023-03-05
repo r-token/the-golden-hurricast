b2ea9133c472c1a96339cd2de5454e3475169571
Ryan Token <ryantoken13@gmail.com>
2023-03-05 08:42:01 -0700

    Hooked up a DynamoDB Stream to forward submitted questions on to Slack

---
+++ serverless/services/questions/stream.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { unmarshall } = require('@aws-sdk/util-dynamodb')
+const fetch = require('node-fetch')
+
+const forwardQuestionToSlack = async (event, context) => {
+	console.log('event:', JSON.stringify(event, null, 2))
+	const webhookUrl = process.env.WEBHOOK_URL
+	console.log('webhookUrl:', webhookUrl)
+	
+	const dynamoRecords = event.Records
+	let unmarshalledRecords = []
+	for (const record of dynamoRecords) {
+		const unmarshalledRecord = unmarshall(record.dynamodb?.NewImage)
+		unmarshalledRecords.push(unmarshalledRecord)
+	}
+	
+	console.log('unmarshalledRecords:', unmarshalledRecords)
+	for (const record of unmarshalledRecords) {
+		const name = record.name !== '' ? record.name : 'Anonymous'
+		const question = record.question
+		const slackObject = {
+			text: `*New question from ${name}*:\n\n ${question}`
+		}
+		
+		try {
+			await fetch(webhookUrl, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(slackObject)
+			})		
+		} catch(err) {
+			console.error('Error posting to Slack:', err)
+			throw err
+		}
+	}
+}
+
+module.exports = {
+	forwardQuestionToSlack
+}
