1b93277316b98efadeb183688ff91dc59d70d3a8
Ryan Token <ryantoken13@gmail.com>
2024-10-27 12:49:46 -0600

    Add 'allowPromotionCodes: true' to the redirectToCheckout Stripe flow

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -77,7 +77,8 @@ const ProductCard = ({ product }) => {
 			lineItems: [{ price: priceId, quantity: Number.parseInt(quantity) }],
 			shippingAddressCollection: {allowedCountries: ['US']},
 			successUrl: `${window.location.origin}/merch-success?productId=${productId}&orderId=${orderId}&quantity=${quantity}`,
-			cancelUrl: `${window.location.origin}/merch`
+			cancelUrl: `${window.location.origin}/merch`,
+			allowPromotionCodes: true
 		})
 
 		if (error) {
